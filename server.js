19407e14d72ed4040992d71bb877e746a9699938
Roblin-Blondeel-Josue <rblondeeljosue@gmail.com>
2023-02-03 13:34:17 +0100

    First Version Work/V2 CORS

--- server.js
+++ server.js
@@ -28,7 +28,9 @@ const corsOptions = {
     "http://localhost:3001",
     "http://localhost:5000",
   ],
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
